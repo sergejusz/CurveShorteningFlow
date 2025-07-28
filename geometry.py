c8fb93a98f97da4693b9f0030e3dd904ea330862
zabinski <sergejus.zabinskis@draeger.com>
2025-07-28 08:38:31 +0300

    Further improvements

--- geometry.py
+++ geometry.py
@@ -5,28 +5,32 @@ from scipy import interpolate
 from numpy.polynomial import polynomial as poly
 import list_operations as list_ops
 import curve_operations as curve_ops
-        
+
 
 def get_curvature(curve, w=5, po=2):
     der1 = signal.savgol_filter(curve, window_length=w, polyorder=po, deriv=1, mode="wrap")
     der2 = signal.savgol_filter(curve, window_length=w, polyorder=po, deriv=2, mode="wrap")
-    return np.divide(np.subtract(np.multiply(der2[1], der1[0]), np.multiply(der2[0], der1[1])), np.power(np.hypot(der1[0], der1[1]), 3))
+    return np.divide(np.subtract(np.multiply(der2[1], der1[0]), np.multiply(der2[0], der1[1])),
+                     np.power(np.hypot(der1[0], der1[1]), 3))
 
 
 def get_tangent_field(curve, w=5, po=1):
     return signal.savgol_filter(curve, window_length=w, polyorder=po, deriv=1, mode="wrap")
 
-   
+
 def get_normal_field(curve, w=3, po=1):
     der1 = get_tangent_field(curve, w, po)
     return np.array([der1[1], -der1[0]])
 
+
 def get_normal_unit_field(curve, w=3, po=1):
     return normalize(get_normal_field(curve, w, po))
-    
+
+
 def normalize(vectors):
-    h = np.hypot(vectors[0],vectors[1])
+    h = np.hypot(vectors[0], vectors[1])
     return np.divide(vectors, np.add(h, np.fabs(np.subtract(np.sign(h), 1.))))
+
 
 def smoothen_curve(curve, w=3, po=2, iter=1):
     if iter == 0:
@@ -35,12 +39,13 @@ def smoothen_curve(curve, w=3, po=2, iter=1):
         curve = signal.savgol_filter(curve, window_length=w, polyorder=po, mode="wrap")
     return curve
 
+
 def shift_curve(curve, index):
     n = curve.shape[1:][0]
     if n == 0:
         return get_empty_curve()
-    idx1 = [(i+index)%n for i in range(0,n)]
-    idx2 = [n + (i+index)%n for i in range(0,n)]
+    idx1 = [(i + index) % n for i in range(0, n)]
+    idx2 = [n + (i + index) % n for i in range(0, n)]
     return np.take(curve, [idx1, idx2])
 
 
@@ -52,31 +57,37 @@ def move_curve_center(curve, x, y):
     [cx, cy] = get_curve_center(curve)
     return translate(curve, x - cx, y - cy)
 
+
 def get_curve_center(curve):
     return np.divide(np.sum(curve, axis=1), curve.shape[1:][0])
+
 
 def get_distances_to_point(x, y, curve):
     return np.hypot(np.subtract(curve[0], x), np.subtract(curve[1], y))
 
+
 def get_sum_distances_to_point(x, y, curve):
     return np.sum(get_distances_to_point(x, y, curve))
 
+
 def get_mean_distances_to_point(x, y, curve):
     if curve_ops.is_empty_curve(curve): return 0
-    return get_sum_distances_to_point(x, y, curve)/curve_ops.get_curve_size(curve)
+    return get_sum_distances_to_point(x, y, curve) / curve_ops.get_curve_size(curve)
 
 
 def homothety_transform(curve, x, y, alpha):
     return np.add(np.multiply(np.subtract(curve, ([x], [y])), alpha), ([x], [y]))
 
+
 def get_curve_length(curve):
     if curve_ops.is_empty_curve(curve): return 0
-    l = np.sum(np.hypot(np.subtract(curve[0][1:], curve[0][:-1]), np.subtract(curve[1][1:],curve[1][:-1]))) 
-    return l + math.hypot(curve[0][0]-curve[0][-1], curve[1][0]-curve[1][-1])
+    l = np.sum(np.hypot(np.subtract(curve[0][1:], curve[0][:-1]), np.subtract(curve[1][1:], curve[1][:-1])))
+    return l + math.hypot(curve[0][0] - curve[0][-1], curve[1][0] - curve[1][-1])
 
 
 def get_curve_steps(curve):
-    return np.append(np.hypot(np.subtract(curve[0][1:], curve[0][:-1]), np.subtract(curve[1][1:],curve[1][:-1])), math.hypot(curve[0][0]-curve[0][-1], curve[1][0]-curve[1][-1]))
+    return np.append(np.hypot(np.subtract(curve[0][1:], curve[0][:-1]), np.subtract(curve[1][1:], curve[1][:-1])),
+                     math.hypot(curve[0][0] - curve[0][-1], curve[1][0] - curve[1][-1]))
 
 
 def get_curve_length_list(curve):
@@ -98,26 +109,26 @@ def get_part_curve_length_from_list(curve_length_list, i, j):
     if n <= 2: return 0
     if i == j: return 0
     if i >= n: return 0
-    
+
     if j < i:
         print("get_part_curve_length len=", n, " i=", i, " j=", j)
-        return curve_length_list[-1] - curve_length_list[i] + curve_length_list[j % (n-1)] - curve_length_list[0]
+        return curve_length_list[-1] - curve_length_list[i] + curve_length_list[j % (n - 1)] - curve_length_list[0]
 
     return curve_length_list[j] - curve_length_list[i]
 
 
 # groups is a list of tuples that contain i1 and i2 - first and last index of point in curve
 def get_excl_curve_length_from_list(curve_length_list, groups):
-	n = len(curve_length_list)
-    
-	l = get_curve_length_from_list(curve_length_list)
-	if len(groups) == 0:
-		return l
+    n = len(curve_length_list)
 
-	s = 0
-	for group in groups:
-		s += get_part_curve_length_from_list(curve_length_list, group[0], group[1])
-	return l - s
+    l = get_curve_length_from_list(curve_length_list)
+    if len(groups) == 0:
+        return l
+
+    s = 0
+    for group in groups:
+        s += get_part_curve_length_from_list(curve_length_list, group[0], group[1])
+    return l - s
 
 
 def get_part_curve_length(curve, i, j):
@@ -131,99 +142,108 @@ def get_part_curve_length(curve, i, j):
     l = 0;
     x1 = curve[0][i % n]
     y1 = curve[1][i % n]
-    for k in range(i+1, j+1):
+    for k in range(i + 1, j + 1):
         x2 = curve[0][k % n]
         y2 = curve[1][k % n]
-        l += math.hypot(x2-x1, y2-y1)
+        l += math.hypot(x2 - x1, y2 - y1)
         x1 = x2
         y1 = y2
     return l
 
+
 def get_excl_curve_length(curve, groups):
-	if curve_ops.is_empty_curve(curve): return 0
-	n = curve_ops.get_curve_size(curve)
-    
-	m = len(groups)
-	l = get_curve_length(curve)
-	if m == 0:
-		return l
+    if curve_ops.is_empty_curve(curve): return 0
+    n = curve_ops.get_curve_size(curve)
 
-	s = 0
-	for i in range(m):
-		s += get_part_curve_length(curve, groups[i][0], groups[i][1])
-	return l - s
+    m = len(groups)
+    l = get_curve_length(curve)
+    if m == 0:
+        return l
 
-    
+    s = 0
+    for i in range(m):
+        s += get_part_curve_length(curve, groups[i][0], groups[i][1])
+    return l - s
+
+
 def get_curvature_over_curve(curve, curvature):
-	if curve_ops.is_empty_curve(curve): return 0.0
-	return np.sum(np.multiply(get_curve_steps(curve), curvature))
+    if curve_ops.is_empty_curve(curve): return 0.0
+    return np.sum(np.multiply(get_curve_steps(curve), curvature))
+
 
 def get_horizontal_amplitude(curve):
-	if curve_ops.is_empty_curve(curve): return 0.0
-	return np.max(curve, axis=1)[0] - np.min(curve, axis=1)[0]
-    
+    if curve_ops.is_empty_curve(curve): return 0.0
+    return np.max(curve, axis=1)[0] - np.min(curve, axis=1)[0]
+
+
 def get_vertical_amplitude(curve):
-	if curve_ops.is_empty_curve(curve): return 0.0
-	return np.max(curve, axis=1)[1] - np.min(curve, axis=1)[1]
+    if curve_ops.is_empty_curve(curve): return 0.0
+    return np.max(curve, axis=1)[1] - np.min(curve, axis=1)[1]
+
+def get_curve_amplitude(curve):
+    return [get_horizontal_amplitude(curve), get_vertical_amplitude(curve)]
 
 # very primitive estimation of radius if curve is circle
 def get_radius_estimation(curve):
-	return (get_horizontal_amplitude(curve) + get_vertical_amplitude(curve))*0.25
+    return (get_horizontal_amplitude(curve) + get_vertical_amplitude(curve)) * 0.25
+
 
 def is_circle(curve):
-	if curve_ops.is_empty_curve(curve): return False
-	pc = get_curve_center(curve)
-	distances = get_distances_to_point(pc[0], pc[1], curve)
-	radius_estimated = list_ops.median_value(distances)
-	s = get_curve_length(curve)
-	radius = s/(2.0*np.pi)
-	# relative tolerance threshold 0.5%
-	threshold = 0.5
-	#print("p=", 100.0*(math.fabs(radius_estimated - radius)/radius_estimated))
-	return 100.0*(math.fabs(radius_estimated - radius)/radius_estimated) <= threshold
+    if curve_ops.is_empty_curve(curve): return False
+    pc = get_curve_center(curve)
+    distances = get_distances_to_point(pc[0], pc[1], curve)
+    radius_estimated = list_ops.median_value(distances)
+    s = get_curve_length(curve)
+    radius = s / (2.0 * np.pi)
+    # relative tolerance threshold 0.5%
+    threshold = 0.5
+    #print("p=", 100.0*(math.fabs(radius_estimated - radius)/radius_estimated))
+    return 100.0 * (math.fabs(radius_estimated - radius) / radius_estimated) <= threshold
+
 
 def resample_by_lsq(curve, w=7, polyorder=2, n=-1):
-	if curve_ops.is_empty_curve(curve): return curve_ops.get_empty_curve()
-	nc = curve_ops.get_curve_size(curve)
-	m = n if n != -1 else nc
-	x_vec = []
-	y_vec = []
-	s = get_curve_length(curve)
-	ds = s/m
-	w2 = w // 2
-	sp = get_curve_length_list(curve)
-    
-	i = 0
-	for k in range(0, m):
-		sk = ds*k
-		if k == 0:
-			i = 0
-		else:
-			while i > 0 and i < nc and not(sk <= sp[i] and sk > sp[i-1]):
-				i += 1
-			if i == nc:
-				if sk > sp[nc-1] and sk <= s:
-					i = nc
-		t = []
-		x = []
-		y = []
-		s0 = 0
-		for j in range(i-w2, i+w2+1):
-			l = j if j<0 else j%nc
-			if j>i-w2 and s0<=0:
-				if t[j-i+w2-1] > sp[l]: s0 = s
-			t.append(sp[l] + s0)
-			x.append(curve[0][l])
-			y.append(curve[1][l])
-		if sk < t[0]: sk += s0
-		p1 = poly.polyfit(t, x, polyorder)
-		x1 = poly.polyval(sk, p1)
-		p2 = poly.polyfit(t, y, polyorder)
-		y1 = poly.polyval(sk, p2)
-		x_vec.append(x1)
-		y_vec.append(y1)
-		if i == 0: i = 1
-	return np.array([x_vec, y_vec])
+    if curve_ops.is_empty_curve(curve): return curve_ops.get_empty_curve()
+    nc = curve_ops.get_curve_size(curve)
+    m = n if n != -1 else nc
+    x_vec = []
+    y_vec = []
+    s = get_curve_length(curve)
+    ds = s / m
+    w2 = w // 2
+    sp = get_curve_length_list(curve)
+
+    i = 0
+    for k in range(0, m):
+        sk = ds * k
+        if k == 0:
+            i = 0
+        else:
+            while i > 0 and i < nc and not (sk <= sp[i] and sk > sp[i - 1]):
+                i += 1
+            if i == nc:
+                if sk > sp[nc - 1] and sk <= s:
+                    i = nc
+        t = []
+        x = []
+        y = []
+        s0 = 0
+        for j in range(i - w2, i + w2 + 1):
+            l = j if j < 0 else j % nc
+            if j > i - w2 and s0 <= 0:
+                if t[j - i + w2 - 1] > sp[l]: s0 = s
+            t.append(sp[l] + s0)
+            x.append(curve[0][l])
+            y.append(curve[1][l])
+        if sk < t[0]: sk += s0
+        p1 = poly.polyfit(t, x, polyorder)
+        x1 = poly.polyval(sk, p1)
+        p2 = poly.polyfit(t, y, polyorder)
+        y1 = poly.polyval(sk, p2)
+        x_vec.append(x1)
+        y_vec.append(y1)
+        if i == 0: i = 1
+    return np.array([x_vec, y_vec])
+
 
 def resample_by_interpolation(curve, n=-1):
     s = get_curve_length(curve)
--- shorten_curve.py
+++ shorten_curve.py
@@ -1,6 +1,5 @@
 import sys
 import os
-#from enum import Enum
 from enum import IntEnum
 import argparse
 import math
@@ -13,11 +12,13 @@ import image_operations as img_ops
 import color_operations as color_ops
 import curve_operations as curve_ops
 
+
 class ViewStyle(IntEnum):
     Invalid = -1
     Contour = 1
     Solid = 2
     Vector = 3
+
 
 class CallbackArgs(IntEnum):
     Rows = 0
@@ -28,23 +29,33 @@ class CallbackArgs(IntEnum):
     BackgroundColor = 5
     ForegroundColor = 6
 
+
 def parse_command_line():
-    parser = argparse.ArgumentParser(prog='shorten_curve.py', description='Curve shortening flow demo', epilog='Text at the bottom of help')
+    parser = argparse.ArgumentParser(prog='shorten_curve.py', description='Curve shortening flow demo',
+                                     epilog='Text at the bottom of help')
     parser.add_argument('imagePath', help='source image file containing closed simple curve')
     parser.add_argument('destFolder', help='Folder path to store output images')
     parser.add_argument('-i', '--iterations', type=int, required=False, default=100, help='max number of iterations')
-    parser.add_argument('-p', '--preserve_length', required=False, action='store_true', help='preserve length')
-    parser.add_argument('-s', '--save_every_n', type=int, required=False, default=5, help='How often image with curve is saved. Default is 5 - every 5th image is saved')
-    parser.add_argument('-v', '--view', required=False, default='contour', choices=['contour', 'solid', 'vector'], help='View style of curves under flow')
-    parser.add_argument('-m', '--median_filter', type=int, required=False, default=0, choices=[3,5,7,9], help='apply median filter for source image with windowsize n')
-    parser.add_argument('-n', '--number_curves', type=int, required=False, default=1, choices=[1,2,3,4,5], help='sets number of curves to apply shortening flow')
-    parser.add_argument('-c', '--color_palette', type=str, required=False, default='blue', choices=['blue','green','red'], help='sets coloring for visualization of filled curves')
-    parser.add_argument('-a', '--save_add_info', required=False, action='store_true', help='save additional information')
+    parser.add_argument('-p', '--preserve_area', required=False, action='store_true', help='preserve area')
+    parser.add_argument('-s', '--save_every_n', type=int, required=False, default=5,
+                        help='How often image with curve is saved. Default is 5 - every 5th image is saved')
+    parser.add_argument('-v', '--view', required=False, default='contour', choices=['contour', 'solid', 'vector'],
+                        help='View style of curves under flow')
+    parser.add_argument('-m', '--median_filter', type=int, required=False, default=0, choices=[3, 5, 7, 9],
+                        help='apply median filter for source image with windowsize n')
+    parser.add_argument('-n', '--number_curves', type=int, required=False, default=1, choices=[1, 2, 3, 4, 5],
+                        help='sets number of curves to apply shortening flow')
+    parser.add_argument('-c', '--color_palette', type=str, required=False, default='blue',
+                        choices=['blue', 'green', 'red'], help='sets coloring for visualization of filled curves')
+    parser.add_argument('-a', '--save_add_info', required=False, action='store_true',
+                        help='save additional information')
     return parser.parse_args()
+
 
 def get_extension(filePath):
     ext = filePath.rpartition('.')[-1]
     return ext
+
 
 def accept_extension(extent):
     ext = extent.strip().lower()
@@ -52,8 +63,27 @@ def accept_extension(extent):
     return len([s for s in ["bmp", "png", "jpg", "jpeg"] if s == ext]) == 1
 
 
+def get_sample_skip_count_for_vector(curve_length, num_samples):
+    ds = curve_length / num_samples
+    if ds < 0.1: return 30
+    if ds < 0.2: return 20
+    if ds < 0.5: return 10
+    if ds < 1.0: return 5
+    if ds < 2.0: return 2
+
+    return 1
+
+def get_vector_scaling_factor(curve):
+    ampl = min(geom.get_curve_amplitude(curve))
+    if ampl >= 60: return 20.0
+    if ampl >= 40: return 12.0
+    if ampl >= 20: return 0.2*ampl + 2.0
+    if ampl >= 10: return 0.3*ampl
+    return 3.0
+
+
 # callback function returns True to terminate flow, False otherwise
-def myCallBackVectorView(curve, curvature, iter, is_circle, obj):
+def myCallBackVectorView(curve, curvature, curve_length, iter, is_circle, obj):
     print("iter=", iter, " curve arc length=", geom.get_curve_length(curve))
 
     if obj is not None:
@@ -63,20 +93,25 @@ def myCallBackVectorView(curve, curvature, iter, is_circle, obj):
             cols = obj[CallbackArgs.Cols]
             file_path = os.path.join(obj[CallbackArgs.Path], 'image' + (str(iter)).zfill(5) + '.png')
             image_exists = os.path.exists(file_path)
-            img =  cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) if image_exists else np.zeros((rows, cols), np.uint8)
+            img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) if image_exists else np.zeros((rows, cols), np.uint8)
             if not img is None:
                 img_ops.draw_curve_lines(img, curve)
                 normal_field = geom.get_normal_field(curve)
                 normal_unit_field = geom.normalize(normal_field)
-                img_ops.display_shortening_field(img, curve, curvature, normal_unit_field, obj[CallbackArgs.ForegroundColor])
+                skip_samples = get_sample_skip_count_for_vector(curve_length, curve_ops.get_curve_size(curve))
+                scaling_factor = get_vector_scaling_factor(curve)
+                img_ops.display_shortening_field(img, curve, curvature, normal_unit_field,
+                                                 obj[CallbackArgs.ForegroundColor], delta_n=skip_samples, magnify = scaling_factor)
                 cv2.imwrite(file_path, img)
-            
+
         max_iterations = obj[CallbackArgs.MaxIterations]
         # terminate flow if number of iterations is exhausted or curve size in horizontal and vertical directions is small
-        return (max_iterations>0 and iter==max_iterations) or (max(geom.get_horizontal_amplitude(curve), geom.get_vertical_amplitude(curve)) < 10)
+        return (max_iterations > 0 and iter == max_iterations) or (
+                    max(geom.get_horizontal_amplitude(curve), geom.get_vertical_amplitude(curve)) < 10)
     return True
 
-def myCallBackContourView(curve, curvature, iter, is_circle, obj):
+
+def myCallBackContourView(curve, curvature, curve_length, iter, is_circle, obj):
     print("iter=", iter, " curve arclength=", geom.get_curve_length(curve))
 
     if obj is not None:
@@ -86,18 +121,20 @@ def myCallBackContourView(curve, curvature, iter, is_circle, obj):
             cols = obj[CallbackArgs.Cols]
             file_path = os.path.join(obj[CallbackArgs.Path], 'image' + (str(iter)).zfill(5) + '.png')
             image_exists = os.path.exists(file_path)
-            img =  cv2.imread(file_path, cv2.IMREAD_COLOR) if image_exists else np.zeros((rows, cols, 3), np.uint8)
+            img = cv2.imread(file_path, cv2.IMREAD_COLOR) if image_exists else np.zeros((rows, cols, 3), np.uint8)
             if not img is None:
                 img_ops.draw_curve_lines(img, curve, obj[CallbackArgs.ForegroundColor])
                 cv2.imwrite(file_path, img)
 
         max_iterations = obj[CallbackArgs.MaxIterations]
         # terminate flow if number of iterations is exhausted or curve size in horizontal and vertical directions is small
-        return (max_iterations>0 and iter==max_iterations) or (max(geom.get_horizontal_amplitude(curve), geom.get_vertical_amplitude(curve)) < 10)
-        
+        return (max_iterations > 0 and iter == max_iterations) or (
+                    max(geom.get_horizontal_amplitude(curve), geom.get_vertical_amplitude(curve)) < 10)
+
     return True
 
-def myCallBackSolidView(curve, curvature, iter, is_circle, obj):
+
+def myCallBackSolidView(curve, curvature, curve_length, iter, is_circle, obj):
     print("iter=", iter, " curve arclength=", geom.get_curve_length(curve))
 
     if obj is not None:
@@ -107,8 +144,8 @@ def myCallBackSolidView(curve, curvature, iter, is_circle, obj):
             cols = obj[CallbackArgs.Cols]
             file_path = os.path.join(obj[CallbackArgs.Path], 'image' + (str(iter)).zfill(5) + '.png')
             image_exists = os.path.exists(file_path)
-            img =  cv2.imread(file_path, cv2.IMREAD_COLOR) if image_exists else np.zeros((rows, cols, 3), np.uint8)
-            
+            img = cv2.imread(file_path, cv2.IMREAD_COLOR) if image_exists else np.zeros((rows, cols, 3), np.uint8)
+
             cv2.floodFill(img, None, (1, 1), obj[CallbackArgs.BackgroundColor])
             if not img is None:
                 img_ops.draw_curve_lines(img, curve, obj[CallbackArgs.ForegroundColor])
@@ -120,9 +157,11 @@ def myCallBackSolidView(curve, curvature, iter, is_circle, obj):
 
         max_iterations = obj[CallbackArgs.MaxIterations]
         # terminate flow if number of iterations is exhausted or curve size in horizontal and vertical directions is small
-        return (max_iterations>0 and iter==max_iterations) or (max(geom.get_horizontal_amplitude(curve), geom.get_vertical_amplitude(curve)) < 10)
-        
+        return (max_iterations > 0 and iter == max_iterations) or (
+                    max(geom.get_horizontal_amplitude(curve), geom.get_vertical_amplitude(curve)) < 10)
+
     return True
+
 
 def extract_curve(img):
     curveExtractor = ce.CurveExtractor()
@@ -132,12 +171,14 @@ def extract_curve(img):
     img_ops.clear_curve(img, curve, 5, 5, 0)
     return curve
 
+
 def save_curve_to_image(curve):
     img = img_ops.create_curve_image(curve)
     rows, cols = img.shape
-    curve = geom.move_curve_center(curve, cols/2, rows/2)
+    curve = geom.move_curve_center(curve, cols / 2, rows / 2)
     img_ops.draw_curve_points(img, curve)
     return img
+
 
 def get_color_palette(coloring_name):
     upper_name = coloring_name.upper()
@@ -149,6 +190,7 @@ def get_color_palette(coloring_name):
         return color_ops.get_green_palette()
     return []
 
+
 def get_background_color(coloring_name):
     upper_name = coloring_name.upper()
     if upper_name == 'RED':
@@ -159,6 +201,7 @@ def get_background_color(coloring_name):
         return color_ops.get_green_background_color()
     return ()
 
+
 def get_view_style(view_style):
     upper_name = view_style.upper()
     if upper_name == 'CONTOUR':
@@ -168,6 +211,7 @@ def get_view_style(view_style):
     if upper_name == 'VECTOR':
         return ViewStyle.Vector
     return ViewStyle.Invalid
+
 
 #
 # reads image from png file and runs curve shortening flow for it
@@ -197,24 +241,23 @@ def main():
     if args.iterations <= 0:
         print("Invalid number of iterations!")
         exit(1)
-    
+
     # load original image
     img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
     assert img is not None, "file could not be read, check with os.path.exists()"
 
     # binarize image and set standard background and foreground colors
     img_ops.binarize(img)
-    
+
     # perform median filtering
     median_img = cv2.medianBlur(img, args.median_filter) if args.median_filter > 0 else img
     if args.median_filter > 0:
         cv2.imwrite(imagePath.replace(extension, "_median.png"), median_img)
-    
+
     # perform curve thinning
     thinned_img = cv2.ximgproc.thinning(median_img)
     cv2.imwrite(imagePath.replace(extension, "_thinned.png"), thinned_img)
-    
-   
+
     # extract curves from image
     curves = []
     curve_cols = []
@@ -231,25 +274,25 @@ def main():
             curve_rows.append(int(min(curve[1])))
             curve_rows.append(int(max(curve[1])))
             curve_img = save_curve_to_image(curve)
-            cv2.imwrite(imagePath.replace(extension, "_extracted" + str(i+1) + ".png"), curve_img)
+            cv2.imwrite(imagePath.replace(extension, "_extracted" + str(i + 1) + ".png"), curve_img)
             num_extracted += 1
         else:
-            print("ERROR: Curve #", i+1, " is not available")
+            print("ERROR: Curve #", i + 1, " is not available")
             break
 
     if num_extracted != args.number_curves:
         print("ERROR: Number of extracted curves = ", num_extracted, " is less than ", args.number_curves)
         return
-    
+
     width = max(curve_cols) - min(curve_cols)
     dx = width // 10 - min(curve_cols)
     width += width // 5
     height = max(curve_rows) - min(curve_rows)
     dy = height // 10 - min(curve_rows)
     height += height // 5
-    
-    view_style =  get_view_style(args.view)
-    
+
+    view_style = get_view_style(args.view)
+
     if view_style == ViewStyle.Solid:
         curve_colors = get_color_palette(args.color_palette)
         if len(curve_colors) == 0:
@@ -270,14 +313,17 @@ def main():
         curve = geom.translate(curves[i], dx, dy)
         curve = geom.smoothen_curve(curve, 3, 1, 100)
         flow = cs.CurveShortener()
-        if args.preserve_length: 
-            flow.set_preserve_curve_length()
+        if args.preserve_area:
+            flow.set_preserve_area()
         if args.save_add_info:
             flow.set_save_additional_info()
-        callBackFcn = myCallBackVectorView if view_style == ViewStyle.Vector else (myCallBackSolidView if view_style == ViewStyle.Solid else myCallBackContourView)
-        flow.setCallBack(callBackFcn, (height, width, args.destFolder, args.iterations, args.save_every_n, background_color, curve_colors[i]))
+        callBackFcn = myCallBackVectorView if view_style == ViewStyle.Vector else (
+            myCallBackSolidView if view_style == ViewStyle.Solid else myCallBackContourView)
+        flow.setCallBack(callBackFcn,
+                         (height, width, args.destFolder, args.iterations, args.save_every_n, background_color,
+                          curve_colors[i]))
         flow.run(curve)
-   
+
 
 if __name__ == "__main__":
     main()
