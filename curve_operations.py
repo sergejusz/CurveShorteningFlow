e363661929e329632afeb68e5a6d326cd76c8760
zabinski <sergejus.zabinskis@draeger.com>
2025-09-03 21:36:16 +0300

    Paperclip curve generation simple optimization

--- curve_operations.py
+++ curve_operations.py
@@ -25,21 +25,21 @@ def get_circle(cx, cy, radius, num_points):
     return get_ellipse(cx, cy, radius, radius, num_points)
 
 def get_paperclip(cx, cy, radius, num_points):
+    # number of points for circular areas
     n1 = int((np.pi * num_points)/(3 + np.pi))
+    # number of points for straight sides
     n2 = num_points - n1
     t1 = np.linspace(1.5 * np.pi, 0.5 * np.pi, n1 // 2, endpoint=False)
     curve = np.array([radius * np.cos(t1), radius * np.sin(t1)])
-    print("First sin=", curve[0][0], curve[1][0], " last =", curve[0][-1], curve[1][-1])
+    #print("First sin=", curve[0][0], curve[1][0], " last =", curve[0][-1], curve[1][-1])
     z1 = np.array([np.linspace(0.0, 3.0*radius, n2 // 2, endpoint=False), np.full(n2 // 2, radius)])
-    print("First z1 = ", z1[0][0], z1[1][0], " last z1=", z1[0][-1], z1[1][-1])
+    #print("First z1 = ", z1[0][0], z1[1][0], " last z1=", z1[0][-1], z1[1][-1])
     t2 = np.linspace(2.5 * np.pi, 1.5 * np.pi, n1 - ( n1 // 2), endpoint=False)
     z2 = np.array([3.0 * radius + radius * np.cos(t2), radius * np.sin(t2)])
-    print("First z2 = ", z2[0][0], z2[1][0], " last z2=", z2[0][-1], z2[1][-1])
+    #print("First z2 = ", z2[0][0], z2[1][0], " last z2=", z2[0][-1], z2[1][-1])
     z3 = np.array([np.linspace(3.0*radius, 0.0, n2 - (n2 // 2), endpoint=False), np.full(n2 - (n2 // 2), -radius)])
-    print("First z3 = ", z3[0][0], z3[1][0], " last z3=", z3[0][-1], z3[1][-1])
-    curve = np.append(curve, z1, axis=1)
-    curve = np.append(curve, z2, axis=1)
-    curve = np.append(curve, z3, axis=1)
-    return np.add(curve, [[cx], [cy]])
+    #print("First z3 = ", z3[0][0], z3[1][0], " last z3=", z3[0][-1], z3[1][-1])
+    #curve = np.append(np.append(np.append(curve, z1, axis=1), z2, axis=1), z3, axis=1)
+    return np.add(np.append(np.append(np.append(curve, z1, axis=1), z2, axis=1), z3, axis=1), [[cx], [cy]])
 
 
